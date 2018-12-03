b3a12b36f2a3a60c88decea300fbd1793de091c6
Nicolas <nicolasvo95@gmail.com>
2018-12-03 15:03:30 +0100

    add model

--- resources/model.py
+++ resources/model.py
@@ -1,10 +1,23 @@
-from flask_restful import Resource
-from sklearn.ensemble import RandomForestClassifier
+from flask_restful import reqparse, abort, Api, Resource
+import pickle
+import numpy as np
+import os
+from resources.nlpmodel import NLPModel
+
+dir_path = os.path.dirname(os.path.abspath(__file__))
+CLASSIFIER = dir_path + "/res/SentimentClassifier.pkl"
+VECTORIZER = dir_path + "/res/TFIDFVectorizer.pkl"
 
 class Model(Resource):
 
     def __init__(self):
-        self.model = RandomForestClassifier()
+
+        self.model = NLPModel()
+        with open(CLASSIFIER, 'rb') as f:
+            self.model.clf = pickle.load(f)
+
+        with open(VECTORIZER, 'rb') as f:
+            self.model.vectorizer = pickle.load(f)
 
     def get(self):
 
